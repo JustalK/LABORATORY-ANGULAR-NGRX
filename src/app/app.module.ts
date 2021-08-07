9ebbc431441aff02e78ae9c9f1c3bef51b63c264
Latsuj <justal.kevin@gmail.com>
2021-08-07 22:17:21 +0800

    [FEATURE] Following the first tuto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import Experience_0001 from '@pages/experience1/experience.component';
+import { counterReducer } from '@pages/experience1/reducers/counter';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 
 @NgModule({
   declarations: [AppComponent, Experience_0001],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({ count: counterReducer })],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/experience1/experience.component.ts
+++ src/app/pages/experience1/experience.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './actions/counter';
 
 @Component({
   templateUrl: './template.component.html',
 })
 export default class CustomComponent {
-  title = 'Secondary';
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
